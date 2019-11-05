2a2bcc0d803b1377e8b09dc25e361509877a174f
Thanh Nguyen <chithanh1811@gmail.com>
2019-11-05 19:49:12 +0100

    format

--- action-jokeIntentTtsParser-joke.py
+++ action-jokeIntentTtsParser-joke.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
+#- * -coding: utf - 8 - * -
 
 from hermes_python.hermes import Hermes
-import json
 import requests
 
 MQTT_IP_ADDR = "localhost"
@@ -11,13 +11,13 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 joke = requests.get("https://sv443.net/jokeapi/category/Any").json()
 
+
 def intent_received(hermes, intent_message):
-	if intent_message.intent.intent_name == 'tellAJoke':
-		if joke["type"] == "single":
-			sentence = joke["joke"]
-		if joke["type"] == "twopart":
-			sentence = joke["setup"] + "      " + joke["delivery"]
-		hermes.publish_end_session(intent_message.session_id, sentence)
-		print(sentence)
+    if joke["type"] == "single":
+        sentence = joke["joke"]
+    else:
+        sentence = joke["setup"] + "      " + joke["delivery"]
+    hermes.publish_end_session(intent_message.session_id, sentence)
+    print(sentence)
 with Hermes(MQTT_ADDR) as h:
-	h.subscribe_intents(intent_received).start()
+    h.subscribe_intents(intent_received).start()
