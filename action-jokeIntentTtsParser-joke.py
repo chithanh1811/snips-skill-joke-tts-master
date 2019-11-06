7ace4098a86ab1df8699e597e70225ef144b4417
chithanh1811 <chithanh1811@gmail.com>
2019-11-06 17:32:26 +0100

    Update action-jokeIntentTtsParser-joke.py

--- action-jokeIntentTtsParser-joke.py
+++ action-jokeIntentTtsParser-joke.py
@@ -4,19 +4,20 @@
 
 from hermes_python.hermes import Hermes
 import requests
+import random
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-joke = requests.get("https://sv443.net/jokeapi/category/any?blacklistFlags=nsfwreligiouspolitical").json()
-
+with open('jokes.json') as json_file:
+    joke = json.load(json_file)
 
 def intent_received(hermes, intent_message):
     if joke["type"] == "single":
-        sentence = joke["joke"]
+        sentence = joke["jokes"][random.randint(0, joke["count"])]["joke"]
     else:
-        sentence = joke["setup"] + "      " + joke["delivery"]
+        sentence = joke["jokes"][random.randint(0, joke["count"])]["setup"] + "      " + joke["jokes"][random.randint(0, joke["count"])]["delivery"]
     hermes.publish_end_session(intent_message.session_id, sentence)
     print(sentence)
 with Hermes(MQTT_ADDR) as h:
