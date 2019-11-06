96c6c917586963f5765442bd60f01a7bfc48fc80
chithanh1811 <chithanh1811@gmail.com>
2019-11-06 18:28:36 +0100

    Update action-jokeIntentTtsParser-joke.py

--- action-jokeIntentTtsParser-joke.py
+++ action-jokeIntentTtsParser-joke.py
@@ -12,13 +12,13 @@ MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 with open('jokes.json') as json_file:
-    joke = json.load(json_file)
+    joke = json.load(json_file)["jokes"][random.randint(0, joke["count"])]
 
 def intent_received(hermes, intent_message):
     if joke["type"] == "single":
-        sentence = joke["jokes"][random.randint(0, joke["count"])]["joke"]
+        sentence = joke["joke"]
     else:
-        sentence = joke["jokes"][random.randint(0, joke["count"])]["setup"] + "      " + joke["jokes"][random.randint(0, joke["count"])]["delivery"]
+        sentence = joke["setup"] + "      " + joke["delivery"]
     hermes.publish_end_session(intent_message.session_id, sentence)
     print(sentence)
 with Hermes(MQTT_ADDR) as h:
